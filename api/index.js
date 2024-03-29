4b34a43906db4e2be9f0b208886344cbaa1e7fde
tuandzvvip <157349218+tuandzvvip@users.noreply.github.com>
2024-03-29 12:25:15 +0700

    change mongoose
    done

--- api/index.js
+++ api/index.js
@@ -10,13 +10,14 @@ const usersRoute = require("./routes/user");
 dotenv.config();
 
 // Kết nối cơ sở dữ liệu MongoDB
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect("mongodb+srv://chutuan2003:tuantran03@cluster0.t7qine3.mongodb.net/")
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
     console.log("Error connecting to MongoDB:", error);
   });
+
 
 app.use(cors());
 app.use(morgan("common"));
